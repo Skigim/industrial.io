99170385c4be6bfdde7b7738cc1d0707f9cb8d27
Skigim <dwight.harr01@outlook.com>
2026-04-30 19:32:43 +0000

    chore: bootstrap workspace tooling

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true, node: true },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["dist", "node_modules"],
+};
