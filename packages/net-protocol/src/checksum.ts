f0847fe9258e50b8d2e94e34b8a9dccf7364e426
Skigim <dwight.harr01@outlook.com>
2026-04-30 19:58:24 +0000

    fix(net-protocol): align checksum with spec

--- packages/net-protocol/src/checksum.ts
+++ packages/net-protocol/src/checksum.ts
@@ -1,22 +1,14 @@
 export function fnv1a32(input: string): number {
   let hash = 0x811c9dc5;
 
-  for (let i = 0; i < input.length; ) {
-    const codePoint = input.codePointAt(i);
-
-    if (codePoint === undefined) {
-      break;
-    }
-
-    hash ^= codePoint;
+  for (let i = 0; i < input.length; i += 1) {
+    hash ^= input.charCodeAt(i);
     hash +=
       (hash << 1) +
       (hash << 4) +
       (hash << 7) +
       (hash << 8) +
       (hash << 24);
-
-    i += codePoint > 0xffff ? 2 : 1;
   }
 
   return hash >>> 0;
